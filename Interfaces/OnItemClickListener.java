bd719b3a0fa80af9f8ef86326c3c1b0df9d0048f
Cameron Wilson <152921039+CamWilsonIX@users.noreply.github.com>
2024-02-20 23:55:33 +0100

    Add files via upload

---
+++ Interfaces/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.irrelevxnce.jblgroundscare.Interfaces;
+
+
+import com.irrelevxnce.jblgroundscare.Model.Alert;
+
+public interface OnItemClickListener {
+    void onItemClick(int position, Alert alert);
+}
